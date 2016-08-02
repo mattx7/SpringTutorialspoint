b6131339e474fbd1bc9f3014cdf42501a539a36f
Lion Pierau <lion.pierau@gmail.com>
2016-08-02 20:24:14 +0200

    copied tutorials from http://www.tutorialspoint.com/spring/spring_hello_world_example.htm

---
+++ SpringTutorialspoint/src/com/tutorialspoint/helloworld/MainApp.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint.helloworld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/tutorialspoint/helloworld/Beans.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+		obj.getMessage();
+	}
+}
